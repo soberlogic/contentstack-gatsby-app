a7039d5ab7a39d99795dee79c26b2f2e15209fce
Amitkanswal <amitkanswal7@gmail.com>
2021-11-11 16:38:17 +0530

    feat:updated env variable [ECO-270]

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,15 +4,9 @@ require("dotenv").config({
   path: `.env.${process.env.NODE_ENV}`,
 })
 
-const {
-  CONTENTSTACK_API_KEY,
-  CONTENTSTACK_DELIVERY_TOKEN,
-  CONTENTSTACK_ENVIRONMENT,
-  CONTENTSTACK_CDN,
-  CONTENTSTACK_HOSTED_URL,
-} = process.env
+const { API_KEY, DELIVERY_TOKEN, ENVIRONMENT, CDN, HOSTED_URL } = process.env
 
-const hostedUrl = CONTENTSTACK_HOSTED_URL || "http://localhost:9000"
+const hostedUrl = HOSTED_URL || "http://localhost:9000"
 
 module.exports = {
   siteMetadata: {
@@ -51,10 +45,10 @@ module.exports = {
     {
       resolve: "gatsby-source-contentstack",
       options: {
-        api_key: CONTENTSTACK_API_KEY,
-        delivery_token: CONTENTSTACK_DELIVERY_TOKEN,
-        environment: CONTENTSTACK_ENVIRONMENT,
-        cdn: CONTENTSTACK_CDN,
+        api_key: API_KEY,
+        delivery_token: DELIVERY_TOKEN,
+        environment: ENVIRONMENT,
+        cdn: CDN,
         // Optional: expediteBuild set this to either true or false
         expediteBuild: true,
         // Optional: Specify true if you want to generate custom schema
