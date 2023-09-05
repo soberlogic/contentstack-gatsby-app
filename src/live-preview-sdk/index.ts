07fce7db03809d910821f5d32d4da641b231a357
Faraaz Biyabani <faraaz.biyabani@contentstack.com>
2023-09-05 12:44:32 +0530

    chore: formatting

--- src/live-preview-sdk/index.ts
+++ src/live-preview-sdk/index.ts
@@ -6,20 +6,20 @@ const isBrowser = typeof window !== "undefined"
 let livePreview: ContentstackGatsby;
 
 if (isBrowser) {
-    livePreview = new ContentstackGatsby({
-        api_key: process.env.CONTENTSTACK_API_KEY,
-        environment: process.env.CONTENTSTACK_ENVIRONMENT,
-        live_preview: {
-            management_token: process.env.CONTENTSTACK_MANAGEMENT_TOKEN,
-            enable: process.env.CONTENTSTACK_LIVE_PREVIEW === "true",
-            host: process.env.CONTENTSTACK_API_HOST,
-        },
-        jsonRteToHtml: true,
-    });
+  livePreview = new ContentstackGatsby({
+    api_key: process.env.CONTENTSTACK_API_KEY,
+    environment: process.env.CONTENTSTACK_ENVIRONMENT,
+    live_preview: {
+      management_token: process.env.CONTENTSTACK_MANAGEMENT_TOKEN,
+      enable: process.env.CONTENTSTACK_LIVE_PREVIEW === "true",
+      host: process.env.CONTENTSTACK_API_HOST,
+    },
+    jsonRteToHtml: true,
+  });
 
-    ContentstackLivePreview.init({
-        stackSdk: livePreview.stackSdk,
-    })
+  ContentstackLivePreview.init({
+    stackSdk: livePreview.stackSdk,
+  })
 }
 
 export { livePreview };
